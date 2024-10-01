65c2e31bdf932ed92066943e5d5166c0521c9072
Uhalov Arseny <akukhalov@gmail.com>
2024-10-01 13:40:23 +0500

    Добавьте файлы проекта.

---
+++ Tyuiu.UhalovAK.Sprint1.Task0.V25/Program.cs
@@ -0,0 +1,34 @@
+﻿using Tyuiu.UhalovAK.Sprint1.Task0.V25.Lib;
+namespace Tyuiu.UhalovAK.Sprint1.Task0.V25
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DataService ds = new DataService();
+            Console.Title = "Спринт #1 | Выполнил: Ухалов А. К. | АСОиУБ-24-1";
+
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* спринт #1                                                               *");
+            Console.WriteLine("* Тема: Базовые навыки работы в C#                                        *");
+            Console.WriteLine("* Задание #0                                                              *");
+            Console.WriteLine("* Вариант #25                                                             *");
+            Console.WriteLine("* Выполнил: Ухалов Арсений Константинович | АСОиУБ-24-1                   *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* УСЛОВИЕ:                                                                *");
+            Console.WriteLine("* Написать программу, которая вычисляет выражение (30 / 6 - 4) * 3        *");
+            Console.WriteLine("* и печатает результат на экране.                                         *");
+            Console.WriteLine("*                                                                         *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+            Console.WriteLine("* (30 / 6 - 4) * 3                                                        *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+            Console.WriteLine("***************************************************************************");
+
+            Console.WriteLine(ds.Calculate());
+
+            Console.ReadLine();
+        }
+    }
+}
