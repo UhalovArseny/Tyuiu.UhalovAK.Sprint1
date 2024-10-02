05b15c954050871a874df36f32c0e48efd463eee
Uhalov Arseny <akukhalov@gmail.com>
2024-10-03 00:02:58 +0500

    Task7

--- Tyuiu.UhalovAK.Sprint1.Task7.V10.Lib/DataService.cs
+++ Tyuiu.UhalovAK.Sprint1.Task7.V10.Lib/DataService.cs
@@ -5,7 +5,7 @@ namespace Tyuiu.UhalovAK.Sprint1.Task7.V10.Lib
     {
         public double Calculate(double x)
         {
-            double res = (2 * (Math.Cos(3 * x) / Math.Sin(3 * x))) - ((Math.Log(x)) / Math.Log(1 + (Math.Pow(x, 2))));
+            double res = (2 * (Math.Cos(3 * x) / Math.Sin(3 * x))) - ((Math.Log(Math.Cos(x))) / Math.Log(1 + (Math.Pow(x, 2))));
             res = Math.Round(res, 3);
             return res;
         }
