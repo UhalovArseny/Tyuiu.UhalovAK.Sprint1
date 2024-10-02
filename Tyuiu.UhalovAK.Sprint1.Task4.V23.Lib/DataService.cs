837d11a376ad1110ce149781494ca6f29e8e4ba8
Uhalov Arseny <akukhalov@gmail.com>
2024-10-02 19:29:46 +0500

    Task4

--- Tyuiu.UhalovAK.Sprint1.Task4.V23.Lib/DataService.cs
+++ Tyuiu.UhalovAK.Sprint1.Task4.V23.Lib/DataService.cs
@@ -5,7 +5,7 @@ namespace Tyuiu.UhalovAK.Sprint1.Task4.V23.Lib
     {
         public double Calculate(double x, double y)
         {
-            double res = (Math.Sqrt(x + y)) / (3 - x);
+            double res = (Math.Sqrt(x + y)) / Math.Abs(3 - x);
             return Math.Round(res, 3);
 
         }
